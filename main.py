2aeb17fa14d27b20f69be344469687683222e10d
jefferson <jefferson2004@gmail.com>
2020-06-11 00:31:22 -0400

    Projeto finalizado

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-'''
-Programa para criar uma calculadora de notas acadêmicas em Python
-
-Dadas diferentes pesos e notas dos alunos, precisamos encontrar as notas finais.
-A pontuação do provas é uma média das respectivas notas (provas, trabalhos e laboratório).
-
-A pontuação final do provas é atribuída usando a fórmula abaixo.
-25% das notas obtidas na submissão de trabalhos
-55% das notas obtidas na prova
-20% das notas obtidas no laboratório
- 
-A nota será calculada de acordo com:
-1. pontuação> = 90: "A."
-2. pontuação> = 80: "B"
-3. pontuação> = 70: "C"
-4. pontuação> = 60: "D"
-
-Além disso, calcule a média total da classe e a nota da classe.
-'''
 from helpers import alunos, calcular_media_total, atribuir_letra_nota, nota_media_classe
 
 if __name__ == '__main__':
